2baa3ed568286bc5a8ba899d9faa4d71617eb7d3
henrymvb <henry.machado@tecnico.ulisboa.pt>
2021-12-26 19:17:08 +0000

    Adicionei o programa de visualização das harmonicas esfericas

---
+++ Programa/Visualizar harmonicas esfericas/visualizar.m
@@ -0,0 +1,46 @@
+clear all
+n=50;
+phi=linspace(0,2*pi,n);
+theta=linspace(0,pi,n);
+
+[PHI, THETA]=meshgrid(phi,theta);
+
+l=1;
+m=0;
+
+a=sqrt(((2*l+1)/(4*pi))*(((factorial(l-m))/(factorial(l+m)))));
+syms s;
+
+M=[(s^2-1)^l];
+G(s)=M;
+N=[(1-s^2)^(m/2)];
+H(s)=N;
+P_ml(s)=((-1)^m/(factorial(l)*(2^l)))*H(s)*diff(G(s),l+m);
+
+
+
+%Com l par, quando R=imag(R), o resultado é zero. mas com l impar 
+%o resultado é a sobreposição de R real e R imag
+
+
+
+R=a.*exp(i*m*PHI).*double(P_ml(cos(THETA)));
+R1=abs(real(R));
+R2=abs(imag(R));
+
+
+x1 = R1 .* sin(THETA) .* cos(PHI);
+y1 = R1.* sin(THETA) .* sin(PHI);
+z1 = R1 .* cos(THETA);
+
+
+ surf(x1,y1,z1);
+ 
+
+
+
+
+
+
+ 
+
